5c9ffd91574c06cb05465d2493b4b597008559cb
xhewr <xhewr123@mailinator.com>
2017-02-13 16:14:58 -0800

    Update lab 2.cpp
    Nothing big here, trimmed down some of our inline comments to each other.  I wrote some comments on switch char choice back to int choice.  Works for me, we just need to add input validation.

--- lab 2.cpp
+++ lab 2.cpp
@@ -31,25 +31,23 @@ int main()
 {
 	// C-string arrays for user input
 	const int LENGTH = 21;
-	char str1[55] = "";         //double length, so str1 can hold str1 + str2
-	char str2[LENGTH] = "";  //  initialized  both arrays filled with \0
+	char str1[55] = "";         //  double length, so str1 can hold str1 + str2
+	char str2[LENGTH] = "";     //  initialized  both arrays filled with \0
 
-	char choice = '0';     //to hold the menu choice from 1-4
-						   //Fabiane: by changing the choice variable to char (instead of int), some things didn't work that well when I tested it
-						   //Example: If I typed 33, it accepted it as menu option 3, when it should have returned the default option
-						   //You must have had a good reason to change it to char, but if not, could we make this an int again?
+	char choice = '0';  //  to hold the menu choice from 1-4
+						//  clifford - going to back int works for me, all we need to do is validate the input to ignore letters and other non numbers
+						//  I remember I did that type of validation in 22a.  I just need to find that bit of code
 
-	int stringNCopyNumber = 0; //Fabiane: this variable isn't used in this scope (DELETE)
-	int count = 0;  //  loop counter //Fabiane: this variable isn't used in this scope (DELETE)
+	int count = 0;  //  clifford - I left this here because its used in more than one case
 
-					// To display the menu and get a choice from user
+	//  display the menu and get a choice from user
 	cout << "Enter the number that corresponds to the string manipulation to be performed:\n\n"
 		<< "    1. String length.\n"
 		<< "    2. String copy\n"
 		<< "    3. String concatenation\n"
 		<< "    4. String comparison.\n\n";
 
-	//To give user the possibility to repeat the menu
+	//  give user the possibility to repeat the menu
 	char again = 'y';
 	do
 	{
@@ -62,8 +60,8 @@ int main()
 		{
 		case '1':  //  stringLength function
 		{
-			inputSingleString(str1, LENGTH);                    // single word input
-			int len = stringLength(str1);                       // return word length to len variable
+			inputSingleString(str1, LENGTH);  //  single word input
+			int len = stringLength(str1);  //  return word length to len variable
 			cout << "Word has " << len << " characters \n";
 			break;
 		}
@@ -71,44 +69,42 @@ int main()
 		case '2':  //  stringNCopy function
 		{
 			char *stringNCopyPointer = nullptr;
-			count = 0;
-			stringNCopyNumber = 0;
+			int stringNCopyNumber = 0;
 			inputSingleString(str2, LENGTH);
 			cout << "enter the number of characters to copy: ";
-			cin >> stringNCopyNumber;
-			//stringNCopy(str1);
+			cin >> stringNCopyNumber;  //  clifford - I need to add in input validation.  To catch letters, negative numbers and other non-number characters
 			stringNCopyPointer = stringNCopy(str1, str2, stringNCopyNumber);
-			cout << "stringNCopy returns: ";  // clifford - added this is in
-
-			while (*(stringNCopyPointer + count) != 0)
+			cout << "stringNCopy returns: ";
+			count = 0;
+			while (*(stringNCopyPointer + count) != '\0' || count < LENGTH)  // clifford - I added this if for what ever reason the loop might try to run forever.  makes me feel better
 			{
 				cout << *(stringNCopyPointer + count);
 				count++;
-			} //Fabiane: Is this loop really needed? For me worked with the C-string name alone.
+			}
 			cout << endl;
 			break;
 		}
 
 		case '3':  //  stringAdd function
 		{
-			inputDoubleString(str1, str2, LENGTH);   // double word input
+			inputDoubleString(str1, str2, LENGTH);  //  double word input
 			char *concatenatedWords = nullptr;
-			concatenatedWords = stringAdd(str1, str2);         // returns concatenated words
+			concatenatedWords = stringAdd(str1, str2);  // returns concatenated words
 			cout << "stringAdd returns: ";
 			count = 0;
-			while (*(concatenatedWords + count) != '\0')  // clifford - added this is in
+			while (*(concatenatedWords + count) != '\0' || count < LENGTH)  // clifford - more infinite loop protections
 			{
 				cout << *(concatenatedWords + count);
 				count++;
-			} //Fabiane: Same as in previous case: is this loop really needed? Worked for me without it.
+			}
 			cout << endl;
 			break;
 		}
 
 		case '4':  //  stringCompare function
 		{
-			inputDoubleString(str1, str2, LENGTH);  // double word input
-			int comp = stringCompare(str1, str2);  // returns value of compared words
+			inputDoubleString(str1, str2, LENGTH);  //  double word input
+			int comp = stringCompare(str1, str2);  //  returns value of compared words
 
 			if (comp == 0)
 				cout << "You inputted identical words.";
@@ -125,15 +121,15 @@ int main()
 		{
 			cout << choice << " is not a choice in this menu.\n" << "You must enter 1, 2, 3 or 4! \n";
 			cin.clear();
-			cin.ignore(10000, '\n');  //Fabiane: had to remove this line completely for keyboard input to work correctly
+			//cin.ignore(10000, '\n');  //Fabiane: had to remove this line completely for keyboard input to work correctly
 		}
 
 
 		}
 
 		cout << "\nManipulate another string? [Y/N]";
-		cin.clear();
-		cin.ignore(10000, '\n'); //Fabiane: had to remove this line same as above
+		//cin.clear();
+		//cin.ignore(10000, '\n'); //Fabiane: had to remove this line same as above
 		cin >> again;
 		cin.clear();
 
@@ -167,31 +163,31 @@ void inputDoubleString(char *string1, char *string2, const int size)
 
 int stringLength(char *charArrayFromMain)
 {
-	// get an array address pointer, then use a loop to count the characters in the array until a null terminator is found
-	// return the length of the array not counting the \0
+	//  get an array address pointer, then use a loop to count the characters in the array until a null terminator is found
+	//  return the length of the array not counting the \0
 
 	int count = 0;
 	while (*(charArrayFromMain + count) != '\0')
 	{
-		count++; // Go to the next char in the string.
+		count++;
 	}
 	return count;
 }
 
 char *stringNCopy(char *destinationCharArray, char *sourceCharArray, int stringNCopyNumber)
 {
-	//  get the address  for the source char array, the destination array, and a number sent from main
+	//  get the address for the source char array, the destination array, and a number sent from main
 	//  run the loop based on the number that was sent from main, or until a \0 is found
 	//  each time the loop runs, copy the character from the source array and put it in the destination array
 	//  add a \0 at the end of the destination array
 	//  return the memory address of the destination array
 
-	int count = 0;  //  counter
+	int count = 0;
 	for (count = 0; count < stringNCopyNumber && (*(sourceCharArray + count) != '\0'); count++)
 	{
 		*(destinationCharArray + count) = *(sourceCharArray + count);
 	}
-	*(destinationCharArray + count) = '\0';  //  Place a null character in 
+	*(destinationCharArray + count) = '\0';
 
 	return destinationCharArray;
 }
